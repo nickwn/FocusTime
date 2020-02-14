9ef18e6f6c85e2b5126b824ca4e4fa849708a807
nick <nick.thenebels@gmail.com>
2020-02-13 16:38:33 -0800

    save current state of master

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var handlebars = require('express3-handlebars');
 var sessions = require('./routes/sessions');
 var event = require('./routes/event');
 var calendar = require('./routes/calendar');
+var finished = require('./routes/finished');
 
 // Create the server instance
 var app = express();
@@ -39,6 +40,7 @@ app.set('view engine', 'handlebars');
 app.get('/calendar', calendar.view);
 app.get('/sessions', sessions.view);
 app.get('/event/:name', event.viewEvent);
+app.get('finished', finished.view);
 
 app.listen(app.get('port'), function() {
 	console.log("Node.js server running on port %s", app.get('port'));
